8691b9d5e80fa1506084825d0a5c9eb8a258414b
Rubiana Perucci <rubi_perucci@hotmail.com>
2023-05-30 21:22:17 -0300

    AULA 2: Obstacles

--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -50,6 +50,7 @@ SMALL_CACTUS = [
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
 ]
+
 LARGE_CACTUS = [
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
